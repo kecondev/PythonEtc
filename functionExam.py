fa6ad69b564e12d9ed65e17ecd0f1e0a90766ef2
LeeSangYong <kecondev@gmail.com>
2025-07-30 13:49:25 +0900

    함수 예제 추가

--- functionExam.py
+++ functionExam.py
@@ -2,4 +2,17 @@ def myFuncSum(a, b):
     """Returns the sum of two numbers."""
     return a + b
 
-print(myFuncSum(3, 5))  # Example usage of the function
+ret = myFuncSum(3, 5)
+print(ret)  # Output the result of the function call
+
+# print(myFuncSum(3, 5))  # Example usage of the function
+
+num = 10
+print("Output:", num)  # Output the value of num
+
+def foo():
+    num = 20
+    print("Inside foo:", num)  # Output the value of num inside foo
+foo()  # Call the function foo to see its output    
+
+print("End of num output:", num)  # Indicate the end of num output
